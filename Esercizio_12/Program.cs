e82b28367036337695e8a20375d8f946c316cf63
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:59:16 +0200

    Add project files.

---
+++ Esercizio_12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Totale spesa:");
+            double spesa = double.Parse(Console.ReadLine());
+            if (spesa < 300)
+            {
+                double sconto = spesa * 10 / 100;
+                double totale = spesa - sconto;
+                Console.WriteLine($"il totale è {totale}");
+            }
+            else if (spesa > 300)
+            {
+                double sconto = spesa * 20 / 100;
+                double totaleMaggiore = spesa - sconto;
+                Console.WriteLine($"lo sconto è {totaleMaggiore}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
